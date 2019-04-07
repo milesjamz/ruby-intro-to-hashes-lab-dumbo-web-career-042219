285286ac9653f88483c6b0d08650dc7493e37830
Miles Marillo <mjm03@hampshire.edu>
2019-04-07 19:43:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,9 +24,15 @@ def monopoly_with_third_tier
 monopoly_with_third_tier = {
   :railroads => {
     :pieces => 4, 
-    :names => {},
+    :names => {
+      :reading_railroad => {}
+    },
+      :reading_railroad
     :rent_in_dollars => {
-      :one_piece_owned => 25
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 100,
+      :four_pieces_owned => 200
     }
   }
 }
